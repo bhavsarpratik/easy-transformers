434a861827a6335bb9ff5fc78f6b3ab168ea09c1
Lint Action <lint-action@samuelmeuli.com>
2021-09-23 05:50:35 +0000

    Fix code style issues with Black

--- easy_transformers/tests/test_loader.py
+++ easy_transformers/tests/test_loader.py
@@ -4,13 +4,22 @@ from easy_transformers.loader import get_model_and_tokenizer, get_pipeline
 
 
 def test_get_model_and_tokenizer_classification(model_name):
-    model, tokenizer = get_model_and_tokenizer(model_name_or_path=model_name, tokenizer_name_or_path=model_name, auto_model_type=AutoModelForSequenceClassification)
+    model, tokenizer = get_model_and_tokenizer(
+        model_name_or_path=model_name,
+        tokenizer_name_or_path=model_name,
+        auto_model_type=AutoModelForSequenceClassification,
+    )
     classifier = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
     prediction = classifier("this is good")
-    assert prediction[0]["label"] == 'LABEL_2'
+    assert prediction[0]["label"] == "LABEL_2"
 
 
 def test_get_pipeline_classification(model_name):
-    classifier = get_pipeline("sentiment-analysis", model_name_or_path=model_name, tokenizer_name_or_path=model_name, auto_model_type=AutoModelForSequenceClassification)
+    classifier = get_pipeline(
+        "sentiment-analysis",
+        model_name_or_path=model_name,
+        tokenizer_name_or_path=model_name,
+        auto_model_type=AutoModelForSequenceClassification,
+    )
     prediction = classifier("this is good")
-    assert prediction[0]["label"] == 'LABEL_2'
+    assert prediction[0]["label"] == "LABEL_2"
